5f2d9be4d0b2553ac87d61dcf1892a2bb2a653be
MarBifrost <128312989+MarBifrost@users.noreply.github.com>
2024-09-27 17:18:40 +0400

    Updated queries.py and added new file query_strings.py

--- queries.py
+++ queries.py
@@ -1,55 +1,38 @@
 from database_generator import DatabaseGenerator
+from query_strings import Queries
 
 
 def mng_queries(db_name):
-    db_conn = DatabaseGenerator(db_name, collection='recipe_collection')
+    db_conn = DatabaseGenerator(database=db_name)
     collection = db_conn.collection
 
-    pipelines = {
-        "average_ingredients": [
-            {"$project": {"ingredients_count": {"$size": "$ingredients"}}},
-            {"$group": {"_id": None, "average_ingredients": {
-                "$avg": "$ingredients_count"}}}
-        ],
-        "average_steps": [
-            {"$project": {"steps_count": {"$size": "$steps"}}},
-            {"$group": {"_id": None, "average_steps": {"$avg": "$steps_count"}}}
-        ],
-        "most_portions": [
-            {"$match": {"portions": {"$type": "array"}}},
-            {"$project": {
-                "recipe_name": 1,
-                "portion_count": {"$toInt": {"$arrayElemAt": [{"$split": ["$portions", " "]}, 0]}}
-            }},
-            {"$sort": {"portion_count": -1}},
-            {"$limit": 1}
-        ],
-        "auth_with_most_recipes": [
-            {"$group": {"_id": "$author", "recipe_count": {"$sum": 1}}},
-            {"$sort": {"recipe_count": -1}},
-            {"$limit": 1}
-        ]
-    }
-
-    results = {}
-    for key, pipeline in pipelines.items():
-        results[key] = list(collection.aggregate(pipeline))
-        if results[key]:
-            print_results(key, pipeline[key])
-
-
-def print_results(key, result):
-    if key == "average_ingredients":
+    result_avg = list(collection.aggregate(Queries.pipeline_avg()))
+    if result_avg:
         print(
-            f"Average number of ingredients: {result[0]['average_ingredients']}")
-    elif key == "average_steps":
-        print(f"Average number of steps: {result[0]['average_steps']}")
-    elif key == "most_portions":
-        print(f"Most portions: {result[0]['recipe_name']}")
-    elif key == "auth_with_most_recipes":
+            f"Average number of ingredients: {
+                result_avg[0]['average_ingredients']}")
+
+    result_avg_steps = list(collection.aggregate(Queries.avg_steps()))
+    if result_avg_steps:
         print(
-            f"The winner is: {result[0]['_id']} with {result[0]['recipe_count']} recipes")
+            f"Average number of stages: {
+                result_avg_steps[0]['average_stages']}")
 
+    result_most_servings = list(collection.aggregate(Queries.most_servings()))
+    if result_most_servings:
+        print(
+            f"Most servings: {
+                result_most_servings[0]['title']}. რეცეპტის ნახვა შეგიძლიათ ლინკზე: {
+                result_most_servings[0]['url']}")
 
-if __name__ == '__main__':
-    mng_queries(db_name="saladsdatabase")
+    result_auth_with_most_recipes = list(
+        collection.aggregate(
+            Queries.author_with_most_recipes()))
+    if result_auth_with_most_recipes:
+        print(
+            f"The winner is: {
+                result_auth_with_most_recipes[0]['_id']} {
+                result_auth_with_most_recipes[0]['recipe_count']} რეცეპტით")
+
+    return result_avg, result_avg_steps, result_avg_steps, result_most_servings, result_auth_with_most_recipes
+
