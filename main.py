dc32e67ca03d90b7269656a8118b3d63fbbc79a5
NikaKhiz <khizambarelinika123@gmail.com>
2024-09-27 19:18:52 +0400

    refactor queries instead of printing results directly from queries py file return results and display them from main pass db connection as parameter instead of recreating it

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ COLLECTION = 'recipe_collection'
 
 async def main():
     database = DatabaseGenerator(client=CLIENT, database=DB, collection_name=COLLECTION)
+    collection = database.collection
     scrapper = Scrapper()
-    queries = mng_queries(DB)
 
     await scrapper.fetch_recipe_urls()
     await scrapper.fetch_salad_recipes()
+    
     database.insert_recipes(scrapper._salads)
+    queries = mng_queries(collection)
+
+    print(f'The average number of ingredients : {queries["avg_num_ingredients"]}')
+    print(f'The average number of stages : {queries["avg_num_stages"]}')
+    print(f'Most servings : {queries["most_servings"]["title"]}. რეცეპტის ნახვა შეგიძლიათ ლინკზე: {queries["most_servings"]["url"]}')
+    print(f'Author with most recipes : {queries["auth_with_most_recipes"]["id"]} with {queries["auth_with_most_recipes"]["recipes"]} recipes.')
 
     await scrapper.close_conn()
 
--- queries.py
+++ queries.py
@@ -1,28 +1,26 @@
-from database_generator import DatabaseGenerator
 from query_strings import Queries
 
 
-def mng_queries(db_name):
-    db_conn = DatabaseGenerator(database=db_name)
-    collection = db_conn.collection
+def mng_queries(collection):
 
     result_avg = list(collection.aggregate(Queries.pipeline_avg()))
-    if result_avg:
-        print(
-            f"Average number of ingredients: {result_avg[0]['average_ingredients']}")
-
     result_avg_steps = list(collection.aggregate(Queries.avg_steps()))
-    if result_avg_steps:
-        print(f"Average number of stages: { result_avg_steps[0]['average_stages']}")
-
     result_most_servings = list(collection.aggregate(Queries.most_servings()))
-    if result_most_servings:
-        print(f"Most servings: {result_most_servings[0]['title']}. რეცეპტის ნახვა შეგიძლიათ ლინკზე: {result_most_servings[0]['url']}")
-
     result_auth_with_most_recipes = list(
         collection.aggregate(
-            Queries.author_with_most_recipes()))
-    if result_auth_with_most_recipes:
-        print(f"The winner is: {result_auth_with_most_recipes[0]['_id']} { result_auth_with_most_recipes[0]['recipe_count']} რეცეპტით")
-
-    return result_avg, result_avg_steps, result_avg_steps, result_most_servings, result_auth_with_most_recipes
+            Queries.author_with_most_recipes()
+        )
+    )
+    
+    return {
+        'avg_num_ingredients': result_avg[0]['average_ingredients'],
+        'avg_num_stages': result_avg_steps[0]['average_stages'],
+        'most_servings': {
+            'title':result_most_servings[0]['title'],
+            'url': result_most_servings[0]['url']
+        },
+        'auth_with_most_recipes': {
+            'id': result_auth_with_most_recipes[0]['_id'],
+            'recipes': result_auth_with_most_recipes[0]['recipe_count']
+        }
+    }
