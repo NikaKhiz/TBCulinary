558e7d4948bb7562474ae4f8292c43ddc5570ea5
NikaKhiz <khizambarelinika123@gmail.com>
2024-09-27 08:55:13 +0400

    modifie utils file change property servings type from int to str concatenate base url with recipe url where needed

--- utils.py
+++ utils.py
@@ -19,17 +19,17 @@ async def fetch_recipe(session, recipe_url, base_url):
         # Parse fetched data
         title = soup.find('div', class_='post__title').find('h1').get_text().strip()
         url = soup.find('div', class_='post__img').find('a')['href']
-        url = f'{base_url}{url.split("next=/")[-1]}'  
+        url = f'{base_url}/{url.split("next=/")[-1]}'  
         category_link, subcategory_link = soup.find_all('a', class_='pagination__item')[2:]
         recipe_image = soup.find('div', class_='post__img').find('img')['src']
-        recipe_description = soup.find('div', class_='post__description').get_text().strip()
+        recipe_description = soup.find('div', class_='post__description').get_text().strip() or 'აღწერა არ არის დამატებული'
         recipe_author = soup.find('div', class_='post__author').find('span').find('a').get_text().strip()
         recipe_servings = soup.find_all('div', class_='lineDesc__item')[1].get_text().strip()
         recipe_ingredients = soup.find_all('div', class_='list__item')
         recipe_stages_list = soup.find_all('div', class_='lineList__item')
         recipe_stages = [
             {
-                'step': int(recipe_stage.find('div').get_text()),
+                'step': recipe_stage.find('div').get_text(),
                 'description': recipe_stage.find('p').get_text()
             }
             for recipe_stage in recipe_stages_list
@@ -51,7 +51,7 @@ async def fetch_recipe(session, recipe_url, base_url):
             'url': url,
             'category': category,
             'subcategory': subcategory,
-            'image': recipe_image,
+            'image': f'{base_url}{recipe_image}',
             'description': recipe_description,
             'author': recipe_author,
             'servings': recipe_servings,
